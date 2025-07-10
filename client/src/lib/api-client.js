8cd3ce9ac8c453b009c97afb033ee744b6d8066d
hjjjjjjjjjjjnnnnnnnnnnnn <israelolalere2008@example.com>
2025-07-10 11:14:32 +0100

    done

--- client/src/lib/api-client.js
+++ client/src/lib/api-client.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 import { HOST } from "../utils/constant";
 
-
-export const  apiClient = axios.create({
-    baseURL: HOST,
-    
+export const apiClient = axios.create({
+  baseURL: HOST,
+  withCredentials: true, // ✅ REQUIRED for cookies to work cross-origin, especially on Safari
 });
-
